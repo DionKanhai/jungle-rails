b3c27b37ab6411bb8273966b52af06ce33cbec32
DionKanhai <dionkanhais@gmail.com>
2023-02-10 04:07:48 +0000

    add count of product/categories to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @total_count_of_products = Product.all.count
+    @total_count_of_categories = Category.all.count
   end
 end
