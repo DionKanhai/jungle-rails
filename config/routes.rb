1992112d64ef270e27e0ce6de341d6698cd013fc
DionKanhai <dionkanhais@gmail.com>
2023-02-14 00:32:52 +0000

    add a signup/login feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   namespace :admin do
     get 'categories/show'
   end
