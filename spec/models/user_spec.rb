0c31c9f81e1b61f821f5379afca3fd614e3d68d3
DionKanhai <dionkanhais@gmail.com>
2023-02-21 21:23:37 +0000

    add users file

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    described_class.new(
+        email: "TEST@TEST.com",
+        first_name: "Billy",
+        last_name: "Jean",
+        password: "lightbulb123",
+        password_confirmation: "lightbulb123"
+      )
+}
+
+  it "is valid with the valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without an email" do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without an first name" do
+    subject.first_name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without an last name" do
+    subject.last_name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without an email" do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without an password" do
+    subject.password = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without an password confirmation" do
+    subject.password_confirmation = nil
+    expect(subject).to_not be_valid
+  end
 end
+
